d8a3d517a50360efa3ee1e6bcb13274ed57b726a
Kim Jansen <kim at xunil.co.nz>
2025-05-13 21:05:55 +1200

    changed to check run as sudo, notify user if not called as sudo

--- install.sh
+++ install.sh
@@ -6,43 +6,52 @@ if [ -z $DISTRO ]; then
     echo "Unknown Distribution, can't install"
     exit
 fi
-echo "Install Xunil Solutions Branding on $DISTRO"
-echo ' '
-echo 'This requires sudo privillages to continue & Plymouth must be installed'
-read -p "Are you sure? " response
+WHO=$(whoami)
+if [ "$WHO" != "root" ]
+then
+    echo ""
+    echo "You must run this as sudo"
+    echo ""
+    echo "eg:  ~ sudo ./install.sh"
+    echo ""
+    exit 0
+fi
+echo "This will install Xunil Solutions Branding on your $DISTRO linux"
+echo ""
+echo "By continuing, you agree you are doing so at your own risk!"
+echo ""
+read -p "Are you sure you want to continue? (y/n) " response
 if [ "$response" != "y" ]; then
     echo 'Aborting Xunil Solutions Branding Installation'
     exit
 fi
 echo ' '
-# We now detect the distro so don't need to ask.
-#read -p "Which Distro are You Running? 1 arch, 2 mint, 3 ubuntu, 4 debian: " distro
 case $DISTRO in
     arch)
       echo 'Installing into Arch Linux'
-      sudo cp --recursive plymouth-theme-xunilsolutions/ /usr/share/plymouth/themes/xunilsolutions/
-      sudo cp configs/archXunilSolutions.xml /usr/share/gnome-background-properties/
-      sudo cp backgrounds/XunilSolutionsArch.jpg /usr/share/backgrounds/XunilSolutions.jpg
-      sudo cp backgrounds/XSArchPlymouth.png /usr/share/plymouth/themes/xunilsolutions/img.png
-      sudo plymouth-set-default-theme -l
-      sudo plymouth-set-default-theme -R xunilsolutions
+      cp --recursive plymouth-theme-xunilsolutions/ /usr/share/plymouth/themes/xunilsolutions/
+      cp configs/archXunilSolutions.xml /usr/share/gnome-background-properties/
+      cp backgrounds/XunilSolutionsArch.jpg /usr/share/backgrounds/XunilSolutions.jpg
+      cp backgrounds/XSArchPlymouth.png /usr/share/plymouth/themes/xunilsolutions/img.png
+      plymouth-set-default-theme -l
+      plymouth-set-default-theme -R xunilsolutions
       echo 'Done...'
       echo 'Your next reboot will show custom boot screen'
       exit
       ;;
     mint) # todo: Detect Mint Flavor, Cinnamon, Mate, XCFE, LMDE etc..
     echo 'Installing for Linux Mint (Cinnamon)'
-      sudo cp backgrounds/XunilSolutions.jpg /usr/share/backgrounds/
-      sudo cp --recursive plymouth-theme-xunilsolutions/ /usr/share/plymouth/themes/xunilsolutions/
-      sudo ln -sfn /usr/share/backgrounds/XunilSolutions.jpg /usr/share/backgrounds/linuxmint/default_background.jpg
-      sudo cp configs/linuxmint.xml /usr/share/cinnamon-background-properties/
-      sudo cp configs/linuxmint-wallpapers.xml /usr/share/cinnamon-background-properties/
+      cp backgrounds/XunilSolutions.jpg /usr/share/backgrounds/
+      cp --recursive plymouth-theme-xunilsolutions/ /usr/share/plymouth/themes/xunilsolutions/
+      ln -sfn /usr/share/backgrounds/XunilSolutions.jpg /usr/share/backgrounds/linuxmint/default_background.jpg
+      cp configs/linuxmint.xml /usr/share/cinnamon-background-properties/
+      cp configs/linuxmint-wallpapers.xml /usr/share/cinnamon-background-properties/
       echo 'Installing plymouth bootloader theme'
-      sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/xunilsolutions/xunilsolutions.plymouth 200
+      update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/xunilsolutions/xunilsolutions.plymouth 200
       echo 'Select the Xunil Theme to continue.'
-      sudo update-alternatives --config default.plymouth
+      update-alternatives --config default.plymouth
       echo 'Rebuilding bootloader to display new theme.'
-      sudo update-initramfs -u
+      update-initramfs -u
       echo 'Done..'
       echo 'Your next reboot will show custom boot screen'
       exit
